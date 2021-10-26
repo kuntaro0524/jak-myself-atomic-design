ffd82ae3b11f5cf932a208830873587ceae372b5
kuntaro0524 <37730000+kuntaro0524@users.noreply.github.com>
2021-10-27 08:35:11 +0900

    UserCard(organisms)を作ったよ UserCardというorgansmsを作ってApp.jsから呼び出す。 とりあえず雛形なので適当な情報を画面に表示しただけ。

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import "./styles.css";
 import { SearchInput } from "./components/molecules/SearchInput";
-
+import { UserCard } from "./components/organisms/user/UserCard";
 export default function App() {
   return (
     <div className="App">
       <SearchInput />
+      <UserCard />
     </div>
   );
 }
---
+++ src/components/organisms/user/UserCard.jsx
@@ -0,0 +1,16 @@
+export const UserCard = () => {
+  return (
+    <div>
+      <img alt="profile" />
+      <p> name </p>
+      <dl>
+        <dt> E-mail </dt>
+        <dd> kuntaro@gmail.com </dd>
+        <dt> URL </dt>
+        <dd> https://kuntaro.com </dd>
+        <dt> Phone </dt>
+        <dd> 080 5319 1649 </dd>
+      </dl>
+    </div>
+  );
+};
