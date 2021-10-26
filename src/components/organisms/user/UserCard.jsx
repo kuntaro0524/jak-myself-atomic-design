b8b5feff5e3f8020e3dbe53e034393fe6be5ed40
kuntaro0524 <37730000+kuntaro0524@users.noreply.github.com>
2021-10-27 08:57:05 +0900

    UserCardの最初の雛形を作成 UserCardの中に画像を埋め込んでstyled componentを設定した。

--- src/components/organisms/user/UserCard.jsx
+++ src/components/organisms/user/UserCard.jsx
@@ -1,16 +1,36 @@
+import styled from "styled-components";
+
 export const UserCard = () => {
   return (
     <div>
-      <img alt="profile" />
+      <img
+        alt="profile"
+        height={160}
+        src="https://www.pngkey.com/png/full/103-1032664_jpg-transparent-download-cute-free-download-mart-girl.png"
+      />
       <p> name </p>
-      <dl>
+      <SDl>
         <dt> E-mail </dt>
         <dd> kuntaro@gmail.com </dd>
         <dt> URL </dt>
         <dd> https://kuntaro.com </dd>
         <dt> Phone </dt>
         <dd> 080 5319 1649 </dd>
-      </dl>
+      </SDl>
     </div>
   );
 };
+
+// テーブルにスタイルを適用する
+const SDl = styled.dl`
+  text-align: left;
+  margin-bottom: 0px;
+  /* dlタグの中のdtタグという設定方法（これは覚えておかないと駄目） */
+  dt {
+    float: left;
+  }
+  dd {
+    padding-left: 32px;
+    padding-bottom: 8px;
+  }
+`;
