64fb7617538e5b009f993ac5710b41fe711cd303
kuntaro0524 <37730000+kuntaro0524@users.noreply.github.com>
2021-10-26 16:42:29 +0900

    共通の部分を切り出す BaseButtonで共通のスタイルを設定して、それをPrimaryButtonの中で使う書き方。 重要なのはPrimaryButton.jsxの書き方ですね。

---
+++ src/components/atoms/button/BaseButton.jsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const BaseButton = styled.button`
+  color: #fff;
+  padding: 6px 24px;
+  border: none;
+  border-radius: 100px;
+  outline: none;
+  &:hover {
+    cursor: pointer;
+    opacity: 0.8;
+  }
+`;
--- src/components/atoms/button/PrimaryButton.jsx
+++ src/components/atoms/button/PrimaryButton.jsx
@@ -1,19 +1,14 @@
 import styled from "styled-components";
+import { BaseButton } from "./BaseButton";
 
 export const PrimaryButton = (props) => {
   const { children } = props;
   return <SButton>{children}</SButton>;
 };
 
-const SButton = styled.button`
-  background-color: #40514e;
-  color: #fff;
-  padding: 6px 24px;
-  border: none;
-  border-radius: 100px;
-  outline: none;
-  &:hover {
-    cursor: pointer;
-    opacity: 0.8;
-  }
+// BaseButtonを別コンポーネントとして準備して、そのコンポーネントに対してパラメタを追加したり上書きしたりする
+const SButton = styled(BaseButton)`
+  background-color: yellow;
+  color: black;
+  padding: 20px 20px;
 `;
