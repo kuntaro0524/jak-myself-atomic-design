c005f74678b5bb337f662a431adde6ff4f00d984
kuntaro0524 <37730000+kuntaro0524@users.noreply.github.com>
2021-10-29 10:38:11 +0900

    templateの作成３ Header.jsxにスタイルを当てていく作業

--- README.md
+++ README.md
@@ -10,3 +10,7 @@ Created with CodeSandbox
 - components/atom/Header.jsx を作成
   ++ `Linkを利用してrouterを設定`
 - components/template/HeaderOnly.jsx から Header 　を読み込む
+
+# template 作成 ３
+
+- 表示に　 styled component を当てていく
--- src/components/atoms/layout/Header.jsx
+++ src/components/atoms/layout/Header.jsx
@@ -1,10 +1,22 @@
 import { Link } from "react-router-dom";
+import styled from "styled-components";
 
 export const Header = () => {
   return (
-    <header>
-      <Link to="/"> HOME </Link>
-      <Link to="/users"> USERS</Link>
-    </header>
+    <SHeader>
+      <SLink to="/"> HOME </SLink>
+      <SLink to="/users"> USERS</SLink>
+    </SHeader>
   );
 };
+
+const SHeader = styled.header`
+  background-color: #11999e;
+  text-align: center;
+  padding: 8px 0;
+`;
+
+// 既存のコンポーネントにスタイルを当てる場合→便利！
+const SLink = styled(Link)`
+  margin: 0 8px;
+`;
