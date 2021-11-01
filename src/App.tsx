7eae37f11ae129dea1a87fb98b67884e836418d1
kuntaro0524 <37730000+kuntaro0524@users.noreply.github.com>
2021-11-01 17:41:50 +0900

    Final version 完成版。少し整形など

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,4 @@
 import "./styles.css";
-import { SearchInput } from "./components/molecules/SearchInput";
-import { UserCard } from "./components/organisms/user/UserCard";
-import { HeaderOnly } from "./components/template/HeaderOnly";
-import { DefaultLayout } from "./components/template/DefaultLayout";
-import { BrowserRouter } from "react-router-dom";
 import { Router } from "./router/Router";
 
 export default function App() {
--- src/components/pages/Top.jsx
+++ src/components/pages/Top.jsx
@@ -1,5 +1,4 @@
 import styled from "styled-components";
-import { DefaultLayout } from "../template/DefaultLayout";
 
 export const Top = () => {
   return (
